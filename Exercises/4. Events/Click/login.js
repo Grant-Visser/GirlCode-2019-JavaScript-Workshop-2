49cfb72fa59e2541f3e1df5fafc357655a3fea09
Julian <julian.mostert@entelect.co.za>
2019-06-09 18:02:15 +0200

    Add events to the codes

---
+++ Exercises/4. Events/Click/login.js
@@ -0,0 +1,10 @@
+//Create a function that will show the email address on the click of the button
+//add the click event to the button
+
+function clickTheButton() {
+    let emailAddress = document.getElementById("email").value
+    if (emailAddress === '') {
+        emailAddress = 'Please type in the email address.'
+    }
+    alert(emailAddress);
+}
