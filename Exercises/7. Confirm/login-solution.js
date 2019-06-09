e39c618b2732af68dac30a257f1c93c2a5d73223
James Reynolds <reynolds.j.a.119@gmail.com>
2019-06-09 17:48:14 +0200

    Moves confirm code to solution file instead

--- Exercises/7. Confirm/login-solution.js
+++ Exercises/7. Confirm/login-solution.js
@@ -1,3 +1,8 @@
-function onSubmit(event) {
-    
+function submitForm(event) {
+    let mustContinue = confirm("Are you sure you want to login?")
+    if (mustContinue === true) {
+        window.location.href = "./next.html"
+    } else  {
+        event.preventDefault();
+    }
 }
--- Exercises/7. Confirm/login.js
+++
@@ -1,8 +0,0 @@
-function submitForm(event) {
-    let mustContinue = confirm("Are you sure you want to login?")
-    if (mustContinue === true) {
-        window.location.href = "./next.html"
-    } else  {
-        event.preventDefault();
-    }
-}
