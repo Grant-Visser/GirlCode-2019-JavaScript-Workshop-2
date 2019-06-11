9bd87df949439ad431a086710cd6e7c92cd31fa8
Grant Visser <grantrvisser@gmail.com>
2019-06-11 21:34:25 +0200

    Adds closure example

--- Exercises/1. Closure/login.js
+++ Exercises/1. Closure/login.js
@@ -1,1 +1,19 @@
-alert("Hello World")
+window.onload = function(e) {
+  alert("There is now HTML displayed!");
+
+  function outerFunction() {
+    let a = 1;
+
+    function innerFunction() {
+      let b = 2;
+      return a + b;
+    }
+
+    return innerFunction();
+  }
+
+  alert(outerFunction());
+
+  alert(a);
+  //Check your console, we can't access 'a' from here!
+};
